8edc6fd3284c6715358dfb7648c7992dd3d6cb13
david joyner <wdjoyner@gmail.com>
2026-03-12 09:53:02 -0400

    initial commit
    copied from https://github.com/swoyer2/manim_chess/tree/main/manim_chess

---
+++ manim_chess/__init__.py
@@ -0,0 +1,12 @@
+from .pieces import (
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
+)
+from .board import Board
+from .evaluation_bar import EvaluationBar
+from .game_player import play_game
+from .game_player import convert_from_PGN
